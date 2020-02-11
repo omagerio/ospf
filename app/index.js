08b73c103581ff725cd556ce5aed89ee1c7a6d55
Omar <omar.frigerio@nextmove.it>
2020-02-11 16:46:36 +0100

    change prod mode

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ https://github.com/omagerio/ospf
     Set to "false" if you want to test your application without compiling or you don't have nodeJS.
     Set to "true" and run "node tools/compile.js" to generate unified sources. Recommended.
 */
-const PRODUCTION_MODE = true;
+const PRODUCTION_MODE = false;
 
 /*
     The version is used to force a refresh of all files. Update accordingly to your app release.
