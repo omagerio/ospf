f8d25a3e2ac8e978be0d8887bcfa9cb1c0ff90b0
Omar (casa) <ruddleshadow@gmail.com>
2020-10-14 12:49:09 +0200

    edit dbmanager: changed localstorage to indexeddb with dexie

--- app/ospf/components/core/DbManager/DbManager.js
+++ app/ospf/components/core/DbManager/DbManager.js
@@ -4,62 +4,28 @@
 class DbManager extends Component {
     async init() {
         await super.init();
-        this.obfuscate = true;
-        this.rot = 7;
-        await this.readFromStorage();
+        this.db = new Dexie("ospf");
     }
 
-    async getValue(key){
-        if(this.db[key] != undefined){
-            return this.db[key];
+    setDbName(name){
+        this.db = new Dexie(name);
+    }
+
+    async setDbStructure(structure){
+        let intVersion = getVersionInt();
+        structure._keyval = "id,val";
+        await this.db.version(intVersion).stores(structure);
+    }
+
+    async getValue(id){
+        let val = await this.db._keyval.get(id);
+        if(val != undefined){
+            return val.val;
         }
         return null;
     }
 
-    async setValue(key, value){
-        this.db[key] = value;
-        await this.saveToStorage();
-    }
-
-    async saveToStorage() {
-        // localStorage.setItem("db", JSON.stringify(this.db));
-        localStorage.setItem("db", await this.ptoo());
-    }
-
-    async ptoo(){
-        let newString = "";
-        let json = JSON.stringify(this.db);
-        let inc = 0;
-        for(let i=0; i<json.length; i++){
-            newString += String.fromCharCode(json.charCodeAt(i) + this.rot + inc);
-            inc++;
-            if(inc > 10){
-                inc = -10;
-            }
-        }
-        return newString;
-    }
-
-    async otop(o){
-        let newString = "";
-        let inc = 0;
-        for(let i=0; i<o.length; i++){
-            newString += String.fromCharCode(o.charCodeAt(i) - this.rot - inc);
-            inc++;
-            if(inc > 10){
-                inc = -10;
-            }
-        }
-        return newString;
-    }
-
-    async readFromStorage() {
-        let db = localStorage.getItem("db");
-        if (db != undefined) {
-            // this.db = JSON.parse(db);
-            this.db = JSON.parse(await this.otop(db));
-        }else{
-            this.db = {};
-        }
+    async setValue(id, val){
+        await this.db._keyval.put({id, val});
     }
 }
--- app/ospf/components/custom/App/App.js
+++ app/ospf/components/custom/App/App.js
@@ -5,6 +5,7 @@ class App extends Component {
     async init({}={}) {
         await super.init(); // always call super.init() first
 
+        // Create the first component to be rendered inside the app
         let layout = await this.addChild("layout", new MenuLayoutExample());
         await layout.init();
     }
