af77ced6613ac3141f9aeb2d2bc11a6b3018ddf6
Omar (casa) <ruddleshadow@gmail.com>
2020-10-22 16:59:10 +0200

    describer update

--- tools/describe.js
+++ tools/describe.js
@@ -2,41 +2,53 @@ let fs = require("fs");
 
 async function init() {
     let config = JSON.parse(fs.readFileSync(__dirname + "/config.json"));
-    let files = walk(__dirname + "/" + config.appFolder);
+    let appConfig = JSON.parse(fs.readFileSync(__dirname + "/" + config.appFolder + "/ospf/config.json", { encoding: "utf8" }));
 
+    let components = appConfig.coreComponents.concat(appConfig.customComponents);
+    components = components.sort();
+
+    let componentsFolder = __dirname + "/" + config.appFolder + "/ospf/components";
     // search for event names
     let eventNames = [];
-    for(let file of files){
-        let fileContents = fs.readFileSync(file, {encoding:"utf8"});
+    for (let componentName of components) {
+        let fileContents = readComponentSource(componentName);
         let regex = /(fireEvent)(.*?)(["'])([a-zA-Z\-_]*)(["'])/g;
 
         let matches = regex.exec(fileContents);
-        if(matches){
-            if(eventNames.indexOf(matches[4]) == -1){
+        if (matches) {
+            if (eventNames.indexOf(matches[4]) == -1) {
                 eventNames.push(matches[4]);
             }
         }
     }
+    eventNames = eventNames.sort();
     console.log("Found events: ", eventNames);
 
     // search for listeners
-    for(let file of files){
-        let fileContents = fs.readFileSync(file, {encoding:"utf8"});
-        for(let eventName of eventNames){
+    for (let componentName of components) {
+        let fileContents = readComponentSource(componentName);
+        let componentEvents = [];
+        for (let eventName of eventNames) {
             let match = fileContents.indexOf(eventName);
-            if(match > -1){
-                console.log(file.replace(__dirname, ""), eventName);
+            if (match > -1) {
+                componentEvents.push(eventName);
             }
         }
+        console.log(componentName + ":", componentEvents);
+    }
 
+    function readComponentSource(componentName) {
+        return fs.readFileSync(componentsFolder + "/" + componentName + "/" + componentName.split("/")[1] + ".js", { encoding: "utf8" });
     }
 
 }
 
-var walk = function(dir) {
+
+
+var walk = function (dir) {
     var results = [];
     var list = fs.readdirSync(dir);
-    list.forEach(function(file) {
+    list.forEach(function (file) {
         file = dir + '/' + file;
         var stat = fs.statSync(file);
         if (stat && stat.isDirectory()) {
