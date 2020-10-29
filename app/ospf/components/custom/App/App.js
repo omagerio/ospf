089447b0998650623c21666ab96a71af8773cdc5
Omar (casa) <ruddleshadow@gmail.com>
2020-10-29 12:39:43 +0100

    updated app.js

--- app/ospf/components/custom/App/App.js
+++ app/ospf/components/custom/App/App.js
@@ -5,6 +5,10 @@ class App extends Component {
     async init({}={}) {
         await super.init(); // always call super.init() first
 
+        // Setting up local database. See: https://dexie.org/
+        app.dbManager.setDbName("demoApp");
+        await app.dbManager.setDbStructure({});
+
         // Create the first component to be rendered inside the app
         let layout = await this.addChild("layout", new MenuLayoutExample());
         await layout.init();
